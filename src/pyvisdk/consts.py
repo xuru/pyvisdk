d329349293651db0fff0927c9763cfa422134408
Eric Plaster <plaster@gmail.com>
2011-02-21 11:16:47 -0600

    Moved most static information off to this consts.py file.

---
+++ src/pyvisdk/consts.py
@@ -0,0 +1,73 @@
+'''
+Created on Feb 15, 2011
+
+@author: eplaster
+'''
+import suds
+
+serviceTypes = {
+    'alarmManager'              : 'AlarmManager',
+    'authorizationManager'      : 'AuthorizationManager',
+    'clusterProfileManager'     : 'ClusterProfileManager',
+    'complianceManager'         : 'ProfileComplianceManager',
+    'customFieldsManager'       : 'CustomFieldsManager',
+    'customizationSpecManager'  : 'CustomizationSpecManager',
+    'diagnosticManager'         : 'DiagnosticManager',
+    'dvSwitchManager'           : 'DistributedVirtualSwitchManager',
+    'eventManager'              : 'EventManager',
+    'extensionManager'          : 'ExtensionManager',
+    'fileManager'               : 'FileManager',
+    'hostProfileManager'        : 'HostProfileManager',
+    'ipPoolManager'             : 'IpPoolManager',
+    'licenseManager'            : 'LicenseManager',
+    'localizationManager'       : 'LocalizationManager',
+    'ovfManager'                : 'OvfManager',
+    'perfManager'               : 'PerformanceManager',
+    'propertyCollector'         : 'PropertyCollector',
+    'rootFolder'                : 'Folder',
+    'scheduledTaskManager'      : 'ScheduledTaskManager',
+    'searchIndex'               : 'SearchIndex',
+    'sessionManager'            : 'SessionManager',
+    'setting'                   : 'OptionManager',
+    'snmpSystem'                : 'HostSnmpSystem',
+    'storageResourceManager'    : 'StorageResourceManager',
+    'taskManager'               : 'TaskManager',
+    'userDirectory'             : 'UserDirectory',
+    'viewManager'               : 'ViewManager',
+    'virtualDiskManager'        : 'VirtualDiskManager',
+    'vmCompatibilityChecker'    : 'VirtualMachineCompatibilityChecker',
+    'vmProvisioningChecker'     : 'VirtualMachineProvisioningChecker',
+}
+
+class TaskInfoState(object):
+    success = "success"
+    running = "running"
+    error = "error"
+    queued = "queued"
+    
+# managed entities
+ManagedEntity = "ManagedEntity" 
+ComputeResource = "ComputeResource"
+ClusterComputeResource = "ClusterComputeResource"
+Datacenter = "Datacenter"
+Folder = "Folder"
+HostSystem = "HostSystem"
+ResourcePool = "ResourcePool"
+VirtualMachine = "VirtualMachine"
+VirtualMachineSnapshot = "VirtualMachineSnapshot"
+
+# compute resources
+ComputeResource = "ComputeResource"
+ClusterComputeResource = "ClusterComputeResource"
+
+# Collectors
+HistoryCollector = "HistoryCollector",
+EventHistoryCollector = "EventHistoryCollector",
+TaskHistoryCollector = "TaskHistoryCollector"
+
+class ManagedObjectRef(suds.sudsobject.Property):
+    """Custom class to replace the suds generated class, which lacks _type."""
+    def __init__(self, _type, value):
+        suds.sudsobject.Property.__init__(self, value)
+        self._type = _type
+        
