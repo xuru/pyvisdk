caa46f064df388f57f8c861194f06abf1dbb13dc
Eric Plaster <plaster@gmail.com>
2011-02-22 11:54:42 -0600

    Now correctly installs the data files

--- setup.py
+++ setup.py
@@ -1,43 +1,61 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 
+# Copyright (c) 2011 Eric Plaster http://ogremountain.com/
+# All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, dis-
+# tribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the fol-
+# lowing conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
+# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
 '''
 pyvisdk - vSphere SDK  for Python
 '''
 
-import os, sys
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
 
-APPDIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, APPDIR)
+from pyvisdk import Version
+from glob import glob
 
-from setuptools import setup, find_packages
+data_files = glob('pyvisdk/wsdl/*')
 
 install_requires = ["suds>=0.3.9"]
 
-setupOptions = {
-    'name': 'pyvisdk',
-    'author': "Eric Plaster",
-    'author_email': "",
-    'description': "vSphere SDK for Python",
-    'long_description': "vSphere SDK for Python",
-    'version': "0.1",
-    'url': "https://github.com/xuru/pyvisdk",
-    'license': "GPL",
-    'install_requires': install_requires,
-    'packages': find_packages(),
-    'classifiers' : ['Development Status :: 4 - Beta',
+setup( name = 'pyvisdk',
+    description = "vSphere SDK for Python",
+    long_description = "vSphere SDK for Python",
+    author = "Eric Plaster",
+    author_email = "plaster@gmail.com",
+    version = Version,
+    url = "https://github.com/xuru/pyvisdk",
+    license = "MIT",
+    install_requires = install_requires,
+    packages = find_packages(),
+    data_files = [('pyvisdk/wsdl', data_files)],
+    classifiers = ['Development Status :: 4 - Beta',
                    'Framework :: VMWare',
                    'Intended Audience :: Developers',
                    'License :: OSI Approved :: MIT License',
                    'Operating System :: OS Independent',
                    'Programming Language :: Python',
-                   'Topic :: Internet :: WWW/HTTP',
-                   'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-                   'Topic :: Internet :: WWW/HTTP :: WSGI',
                    'Topic :: Software Development :: Libraries :: Python Modules',
                    ],
-    }
-
-if __name__ == '__main__':
-    setup(**setupOptions)
+    )
 
 
