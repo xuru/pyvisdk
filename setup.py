c8485a4e8160a8782dde800393c30b094fa113be
Eric Plaster <plaster@gmail.com>
2011-02-25 09:00:22 -0600

    Updated the setup.py to include all the libraries and includes that vixDiskLib needs

--- setup.py
+++ setup.py
@@ -25,17 +25,62 @@
 pyvisdk - vSphere SDK  for Python
 '''
 
-try:
-    from setuptools import setup, find_packages
-except ImportError:
-    from distutils.core import setup
-
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import subprocess, os, time
 from pyvisdk import Version
 from glob import glob
 
+#################################################################################
+# Helper functions for setup
+#################################################################################
+def run(cmd, wait=True, timeout=30):
+    result = ""
+
+    p = subprocess.Popen(cmd, shell=False, env=os.environ, stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    if wait:
+        result = p.communicate("\n")[0].strip()
+        if p.returncode != 0:
+            raise RuntimeError('Error executing %r: %r' % (cmd, result))
+    else:
+        p.stdin.write("\n")
+        for x in range(timeout):
+            rc = p.poll()
+            if rc == None:
+                # process hasn't terminated yet...
+                print "%d seconds and still running..." % x
+                time.sleep(1)
+            else:
+                break
+    return result.strip()
+
+def run_pkg_config(packages):
+    args = ['pkg-config', '--libs', '--cflags'] + list(packages)
+    return run(args)
+
+def pkgconfig(*packages, **kw):
+    flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
+    for token in run_pkg_config(packages).split():
+        kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
+    return kw
+
+#################################################################################
+# Setup the libraries and include paths for vixDiskLib (VDDK)
+#################################################################################
+kw = pkgconfig('vix-disklib')
+# we need to make sure that we include the vixDiskLibVim library
+kw['libraries'].append('vixDiskLibVim')
+print "kw: ", kw
+vddk_module = Extension('pyvisdk.vix.vddk', ['pyvisdk/vix/vddk.pyx'], **kw)
+
+# include the WSDL
 data_files = glob('pyvisdk/wsdl/*')
 
-install_requires = ["suds>=0.3.9"]
+# we need to make sure we have these to python modules in our path
+install_requires = ["suds", "cython"]
+
 
 setup( name = 'pyvisdk',
     description = "vSphere SDK for Python",
@@ -45,8 +90,10 @@ setup( name = 'pyvisdk',
     version = Version,
     url = "https://github.com/xuru/pyvisdk",
     license = "MIT",
-    install_requires = install_requires,
-    packages = find_packages(),
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [vddk_module],
+    requires = install_requires,
+    packages = ["pyvisdk"],
     data_files = [('pyvisdk/wsdl', data_files)],
     classifiers = ['Development Status :: 4 - Beta',
                    'Framework :: VMWare',
