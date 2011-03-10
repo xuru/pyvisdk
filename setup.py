814ba988e330d93ac3843509307230461125624f
Eric Plaster <plaster@gmail.com>
2011-03-10 13:42:07 -0600

    The vixDiskLib stuff was moved to it's own module

--- setup.py
+++ setup.py
@@ -26,61 +26,14 @@ pyvisdk - vSphere SDK  for Python
 '''
 
 from distutils.core import setup
-from Cython.Distutils import extension, build_ext
-import subprocess, os, time
 from pyvisdk import Version
 from glob import glob
-
-#################################################################################
-# Helper functions for setup
-#################################################################################
-def run(cmd, wait=True, timeout=30):
-    result = ""
-
-    p = subprocess.Popen(cmd, shell=False, env=os.environ, stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    if wait:
-        result = p.communicate("\n")[0].strip()
-        if p.returncode != 0:
-            raise RuntimeError('Error executing %r: %r' % (cmd, result))
-    else:
-        p.stdin.write("\n")
-        for x in range(timeout):
-            rc = p.poll()
-            if rc == None:
-                # process hasn't terminated yet...
-                print "%d seconds and still running..." % x
-                time.sleep(1)
-            else:
-                break
-    return result.strip()
-
-def run_pkg_config(packages):
-    args = ['pkg-config', '--libs', '--cflags'] + list(packages)
-    return run(args)
-
-def pkgconfig(*packages, **kw):
-    flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
-    for token in run_pkg_config(packages).split():
-        kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
-    return kw
-
-#################################################################################
-# Setup the libraries and include paths for vixDiskLib (VDDK)
-#################################################################################
-kw = pkgconfig('vix-disklib')
-# we need to make sure that we include the vixDiskLibVim library
-kw['libraries'].append('vixDiskLibVim')
-kw['pyrex_gdb'] = True  # enable debugging
-print "kw: ", kw
-vddk_module = extension.Extension('pyvisdk.vix.vixDiskLib', ['pyvisdk/vix/vixDiskLib.pyx'], **kw)
 
 # include the WSDL
 data_files = glob('pyvisdk/wsdl/*')
 
 # we need to make sure we have these to python modules in our path
-install_requires = ["suds", "cython"]
-
+install_requires = ["suds"]
 
 setup( name = 'pyvisdk',
     description = "vSphere SDK for Python",
@@ -90,8 +43,6 @@ setup( name = 'pyvisdk',
     version = Version,
     url = "https://github.com/xuru/pyvisdk",
     license = "MIT",
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [vddk_module],
     requires = install_requires,
     packages = ["pyvisdk"],
     data_files = [('pyvisdk/wsdl', data_files)],
