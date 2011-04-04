07298763ce3867bb08909b8c62e0fd3f64dd1307
Eric Plaster <plaster@gmail.com>
2011-04-04 18:02:45 -0500

    fixed the way managed objects are generated

--- pyvisdk/managedObjects/managedentity.py
+++ pyvisdk/managedObjects/managedentity.py
@@ -3,17 +3,18 @@ Created on Mar 8, 2011
 
 @author: eplaster
 '''
+from pyvisdk import consts
 from pyvisdk.consts import ManagedEntityTypes
 from pyvisdk.core import ManagedObjectReference
+from pyvisdk.managedObjects import ManagedEntities
 import logging
-import time
 import types
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 
 
-class TaskDelegate:
+class TaskDelegate(object):
     """ delegates to the real method, and waits for the task to complete"""
     
     def __init__(self, cls, name):
@@ -33,17 +34,19 @@ class TaskDelegate:
             args = (self.ref, )
         rv = self.__target(*args, **kwargs)
         if self.name[-5:] == '_Task':
-            print "Waiting for task to Complete"
+            log.debug("Waiting for task to Complete")
             self.core.waitForTask(rv)
         return rv
-    
+   
 class ManagedEntity(object):
+    __slots__ = ['props']
     def __init__(self, core, methods=[], props=[], name=None, ref=None):
         self.ref = ref
         self.core = core
         self.client = core.client
         self.service = core.client.service
-        self.name = name
+        if name:
+            self.name = name
         self.props = ["configIssue", "configStatus", "name", "parent"] + props
         
         if not getattr(self, 'type'):
@@ -65,31 +68,44 @@ class ManagedEntity(object):
         if type(prop) == types.ListType:
             for x in prop:
                 self.update(x)
-        
+                
         data = self.core.getDynamicProperty(self.ref, prop)
         for name, value in data.items():
             try:
                 if name in self.props:
                     log.debug("[%s] %s" % (name, value.__class__.__name__))
-                    self.__dict__[name] = value
+                    self.__dict__[name] = self._clean(value)
             except AttributeError, e:
                 log.warning("[WARNING] [%s] %s" %  (name, e))
                 
+    def _clean(self, objectContent):
+        if type(objectContent) == types.ListType:
+            out = []
+            for x in objectContent:
+                out.append(self._clean(x))
+            return out
+        elif objectContent.__class__.__name__ == "ObjectContent":
+            obj = objectContent.obj
+            if obj._type in consts.ManagedEntityTypes:
+                return ManagedEntities[obj._type](self, objectContent)
+        elif objectContent.__class__.__name__ == "ManagedObjectReference" and objectContent._type in ManagedEntityTypes:
+            return ManagedEntities[objectContent._type](self.core, objectContent)
+        else:
+            log.debug("Unable to clean: %s" % objectContent)
+        return objectContent
     
-    def __getattribute__( self, _name ):
-        """
-        Return a proxy wrapper object if this is a method call.
-        """
-        try:
-            return object.__getattribute__(self, _name)
-        except AttributeError:
-            try:
-                return object.__getattribute__(self, _name+"_Task")
-            except AttributeError:
-                if _name in self.props:
-                    self.update(_name)
-                    time.sleep(1)
-                    return object.__getattribute__(self, _name)
-                else:
-                    raise
+    def __getattr__(self, _name):
+        if _name in self.props:
+            self.update(_name)
+        return object.__getattribute__(self, _name)
+
+
+
+
+
+
+
+
+
+
 
