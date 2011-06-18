bb957db0170bdd2626e6eb2da2a5412478836a35
Eric Plaster <plaster@gmail.com>
2011-06-18 15:33:52 -0500

    Better Enum class

--- pyvisdk/consts.py
+++ pyvisdk/consts.py
@@ -3,16 +3,68 @@ Created on Feb 15, 2011
 
 @author: eplaster
 '''
-from pyvisdk.utiils import Enum
+import collections
 
-# enums
-TaskInfoState = Enum("success", "running", "error", "queued")
-ManagedEntityTypes = Enum( "ManagedEntity", "ComputeResource", "ClusterComputeResource", "Datacenter", "Folder", 
+class Enum(collections.Mapping):
+    """Enum is a subclass of :class:`collections.Mapping`, implementing comparison
+    methods adhering to precedence rules."""
+    
+    def __init__(self, *argv, **kwargs):
+        super(Enum, self).__init__(*argv, **kwargs)
+        self.data = {}
+
+    def labels(self, *args, **kwargs):
+        for item in args:
+            self.data[item] = item
+        for n,v in kwargs.items():
+            self.data[n] = v
+        return self
+        
+    def compare(self, other, fun, default=False):
+        return fun(self.precedence(self), self.precedence(other))
+
+    def __gt__(self, other):
+        return self.compare(other, lambda a, b: a < b, True)
+
+    def __ge__(self, other):
+        return self.compare(other, lambda a, b: a <= b, True)
+
+    def __lt__(self, other):
+        return self.compare(other, lambda a, b: a > b, False)
+
+    def __le__(self, other):
+        return self.compare(other, lambda a, b: a >= b, False)
+    
+    def __len__(self):
+        return len([x for x in self.data.values() if x[0] != "_"])
+    
+    def __iter__(self):
+        return iter(self.data)
+    
+    def __getitem__(self, item):
+        return self.data[item]
+    
+    def __getattr__(self, item):
+        return self.data[item]
+    
+    def precedence(self, item):
+        """Get the precedence index for item.
+    
+        Lower index means higher precedence.
+    
+        """
+        try:
+            return self.PRECEDENCE.index(item)
+        except ValueError:
+            return self.PRECEDENCE.index(None)
+        
+TaskInfoState = Enum().labels("success", "error", "running", "queued")
+ManagedEntityTypes = Enum().labels( "ManagedEntity", "ComputeResource", "ClusterComputeResource", "Datacenter", "Folder", 
         "HostSystem", "ResourcePool", "VirtualMachine", "VirtualMachineSnapshot", "Datastore")
-ComputeResourcesTypes = Enum("ComputeResource","ClusterComputeResource")
-HistoryCollectorTypes = Enum("HistoryCollector", "EventHistoryCollector", "TaskHistoryCollector")
+ComputeResourcesTypes = Enum().labels("ComputeResource","ClusterComputeResource")
+HistoryCollectorTypes = Enum().labels("HistoryCollector", "EventHistoryCollector", "TaskHistoryCollector")
 
-serviceTypes = {
+serviceTypes = Enum().labels( **{
     'alarmManager'              : 'AlarmManager',
     'authorizationManager'      : 'AuthorizationManager',
     'clusterProfileManager'     : 'ClusterProfileManager',
@@ -44,5 +96,5 @@ serviceTypes = {
     'virtualDiskManager'        : 'VirtualDiskManager',
     'vmCompatibilityChecker'    : 'VirtualMachineCompatibilityChecker',
     'vmProvisioningChecker'     : 'VirtualMachineProvisioningChecker',
-}
+} )
     
