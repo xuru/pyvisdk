88a04319b39d0d2b63ab284b509716cf8222951b
Eric Plaster <plaster@gmail.com>
2011-07-06 12:29:51 -0500

    core tests now work correctly

--- tests/test.py
+++ tests/test.py
@@ -3,35 +3,38 @@ Created on Feb 11, 2011
 
 @author: eplaster
 '''
+
 import unittest
 import os.path
 import sys
+from tests.common import get_options
 
 here = os.path.expanduser(os.path.dirname(__file__))
-sys.path.insert(0, os.path.join("..", "src"))
+sys.path.insert(0, os.path.join(here, ".."))
 
 from pyvisdk import Vim
 
-ident = open("ident.cnf").readlines()
-server = ident[0].strip()
-username = ident[1].strip()
-password = ident[2].strip()
-
 class Test(unittest.TestCase):
+    def setUp(self):
+        self.options = get_options()
+
+    def tearDown(self):
+        pass
+    
     def testConnection(self):
-        vim = Vim(server, verbose=0, connect=False)
+        vim = Vim(self.options.server, verbose=0, connect=False)
         assert(vim.connected == False)
         
         vim.connect()
         assert(vim.connected == True)
     
     def testAbout(self):
-        vim = Vim(server, verbose=0)
+        vim = Vim(self.options.server, verbose=0)
         vim.displayAbout()
 
     def testLogin(self):
-        vim = Vim(server, verbose=0)
-        vim.login(username, password)
+        vim = Vim(self.options.server, verbose=0)
+        vim.login(self.options.username, self.options.password)
         assert(vim.loggedin == True)
         vim.logout()
 
