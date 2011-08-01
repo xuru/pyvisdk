45b445cce86a470fe3be88187786f02c38336901
Eric Plaster <plaster@gmail.com>
2011-08-01 17:22:38 -0500

    Moved the Enum class into the utils.py file

--- pyvisdk/consts.py
+++ pyvisdk/consts.py
@@ -3,68 +3,13 @@ Created on Feb 15, 2011
 
 @author: eplaster
 '''
-import collections
+from pyvisdk.utils import Enum        
 
-class Enum(collections.Mapping):
-    """Enum is a subclass of :class:`collections.Mapping`, implementing comparison
-    methods adhering to precedence rules."""
-    
-    def __init__(self, *argv, **kwargs):
-        super(Enum, self).__init__(*argv, **kwargs)
-        self.data = {}
+TaskInfoState = Enum("success", "error", "running", "queued")
+ComputeResourcesTypes = Enum("ComputeResource","ClusterComputeResource")
+HistoryCollectorTypes = Enum("HistoryCollector", "EventHistoryCollector", "TaskHistoryCollector")
 
-    def labels(self, *args, **kwargs):
-        for item in args:
-            self.data[item] = item
-        for n,v in kwargs.items():
-            self.data[n] = v
-        return self
-        
-    def compare(self, other, fun, default=False):
-        return fun(self.precedence(self), self.precedence(other))
-
-    def __gt__(self, other):
-        return self.compare(other, lambda a, b: a < b, True)
-
-    def __ge__(self, other):
-        return self.compare(other, lambda a, b: a <= b, True)
-
-    def __lt__(self, other):
-        return self.compare(other, lambda a, b: a > b, False)
-
-    def __le__(self, other):
-        return self.compare(other, lambda a, b: a >= b, False)
-    
-    def __len__(self):
-        return len([x for x in self.data.values() if x[0] != "_"])
-    
-    def __iter__(self):
-        return iter(self.data)
-    
-    def __getitem__(self, item):
-        return self.data[item]
-    
-    def __getattr__(self, item):
-        return self.data[item]
-    
-    def precedence(self, item):
-        """Get the precedence index for item.
-    
-        Lower index means higher precedence.
-    
-        """
-        try:
-            return self.PRECEDENCE.index(item)
-        except ValueError:
-            return self.PRECEDENCE.index(None)
-        
-TaskInfoState = Enum().labels("success", "error", "running", "queued")
-ManagedEntityTypes = Enum().labels( "ManagedEntity", "ComputeResource", "ClusterComputeResource", "Datacenter", "Folder", 
-        "HostSystem", "ResourcePool", "VirtualMachine", "VirtualMachineSnapshot", "Datastore")
-ComputeResourcesTypes = Enum().labels("ComputeResource","ClusterComputeResource")
-HistoryCollectorTypes = Enum().labels("HistoryCollector", "EventHistoryCollector", "TaskHistoryCollector")
-
-serviceTypes = Enum().labels( **{
+serviceTypes = Enum( **{
     'alarmManager'              : 'AlarmManager',
     'authorizationManager'      : 'AuthorizationManager',
     'clusterProfileManager'     : 'ClusterProfileManager',
