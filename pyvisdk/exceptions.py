b1ed997c7c5a14a437f18094546b35e077d47fa8
Eric Plaster <plaster@gmail.com>
2011-08-01 17:24:17 -0500

    Moved exceptions into their own file

---
+++ pyvisdk/exceptions.py
@@ -0,0 +1,14 @@
+'''
+Created on Jul 29, 2011
+
+@author: eplaster
+'''
+
+class PyVisdkError(Exception):
+    pass
+
+class VisdkTaskError(Exception):
+    pass
+
+class VisdkInvalidState(Exception):
+    pass
