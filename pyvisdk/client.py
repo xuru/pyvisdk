e3ebe7760153318e544e847cb0dd032c402bb236
Eric Plaster <plaster@gmail.com>
2011-08-01 17:21:54 -0500

    Cleaned up the code a bit Moved the Delegate class out

--- pyvisdk/client.py
+++ pyvisdk/client.py
@@ -3,42 +3,15 @@ Created on Mar 6, 2011
 
 @author: eplaster
 '''
-from pyvisdk import consts
-from pyvisdk.managedObjects import ManagedEntities
+from pyvisdk.consts import ComputeResourcesTypes, HistoryCollectorTypes
+from pyvisdk.mo.consts import ManagedEntityTypes
 from suds import MethodNotFound
 import logging
 import os.path
 import suds
-import types
 
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
-
-class Delegate:
-    """ delegates to the real method, and cleans up the return value """
-    
-    def __init__(self, method):
-        """ Initializes the delagate with the method to proxy """
-        self.__target = method
-        
-    def __call__(self, *args, **kwargs):
-        """ calls original method """
-        log.debug("Calling %s" % self.__target.method.name)
-        
-        return self.clean(self.__target(*args, **kwargs))
-
-    def clean(self, objectContent):
-        if type(objectContent) == types.ListType:
-            out = []
-            for x in objectContent:
-                out.append(self.clean(x))
-            return out
-        elif objectContent.__class__.__name__ == "ObjectContent":
-            obj = objectContent.obj
-            if obj._type in consts.ManagedEntityTypes:
-                return ManagedEntities[obj._type](self, objectContent)
-        return objectContent
-            
 
 class Client(object):
     '''
@@ -70,7 +43,7 @@ class Client(object):
         try:
             _attr = getattr(service, attr)
             if _attr.__class__ == suds.client.Method:
-                #return Delegate(_attr)
+                # we need to do something about getting to the right method here...
                 return _attr
             else:
                 return _attr
@@ -89,30 +62,15 @@ class Client(object):
         if searchType == foundType:
             return True
         elif searchType == "ManagedEntity":
-            for me in consts.ManagedEntityTypes:
+            for me in ManagedEntityTypes:
                 if me == foundType:
                     return True
         elif searchType == "ComputeResource":
-            for me in consts.ComputeResourcesTypes:
+            for me in ComputeResourcesTypes:
                 if me == foundType:
                     return True
         elif searchType == "HistoryCollector":
-            for me in consts.HistoryCollectorTypes:
+            for me in HistoryCollectorTypes:
                 if me == foundType:
                     return True
         return False
-
-    #def __delattr__(self, name):
-    #    delattr(object.__getattribute__(self, "service"), name)
-        
-    #def __setattr__(self, name, value):
-    #    setattr(object.__getattribute__(self, "service"), name, value)
-    
-    #def __nonzero__(self):
-    #    return bool(object.__getattribute__(self, "service"))
-    
-    #def __str__(self):
-    #    return str(object.__getattribute__(self, "service"))
-    
-    #def __repr__(self):
-    #    return repr(object.__getattribute__(self, "service"))
