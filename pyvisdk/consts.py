41b293181a3c57d63209bed426164778b45db7b8
Eric Plaster <plaster@gmail.com>
2011-08-09 10:34:17 -0500

    Removed serviceTypes, ComputeResourceTypes and HistoryCollectorTypes enums.  Left TaskInfoState until we can bring across enum types from the API.

--- pyvisdk/consts.py
+++ pyvisdk/consts.py
@@ -3,43 +3,6 @@ Created on Feb 15, 2011
 
 @author: eplaster
 '''
-from pyvisdk.utils import Enum        
+from pyvisdk.thirdparty import Enum        
 
 TaskInfoState = Enum("success", "error", "running", "queued")
-ComputeResourcesTypes = Enum("ComputeResource","ClusterComputeResource")
-HistoryCollectorTypes = Enum("HistoryCollector", "EventHistoryCollector", "TaskHistoryCollector")
-
-serviceTypes = Enum( **{
-    'alarmManager'              : 'AlarmManager',
-    'authorizationManager'      : 'AuthorizationManager',
-    'clusterProfileManager'     : 'ClusterProfileManager',
-    'complianceManager'         : 'ProfileComplianceManager',
-    'customFieldsManager'       : 'CustomFieldsManager',
-    'customizationSpecManager'  : 'CustomizationSpecManager',
-    'diagnosticManager'         : 'DiagnosticManager',
-    'dvSwitchManager'           : 'DistributedVirtualSwitchManager',
-    'eventManager'              : 'EventManager',
-    'extensionManager'          : 'ExtensionManager',
-    'fileManager'               : 'FileManager',
-    'hostProfileManager'        : 'HostProfileManager',
-    'ipPoolManager'             : 'IpPoolManager',
-    'licenseManager'            : 'LicenseManager',
-    'localizationManager'       : 'LocalizationManager',
-    'ovfManager'                : 'OvfManager',
-    'perfManager'               : 'PerformanceManager',
-    'propertyCollector'         : 'PropertyCollector',
-    'rootFolder'                : 'Folder',
-    'scheduledTaskManager'      : 'ScheduledTaskManager',
-    'searchIndex'               : 'SearchIndex',
-    'sessionManager'            : 'SessionManager',
-    'setting'                   : 'OptionManager',
-    'snmpSystem'                : 'HostSnmpSystem',
-    'storageResourceManager'    : 'StorageResourceManager',
-    'taskManager'               : 'TaskManager',
-    'userDirectory'             : 'UserDirectory',
-    'viewManager'               : 'ViewManager',
-    'virtualDiskManager'        : 'VirtualDiskManager',
-    'vmCompatibilityChecker'    : 'VirtualMachineCompatibilityChecker',
-    'vmProvisioningChecker'     : 'VirtualMachineProvisioningChecker',
-} )
-    
