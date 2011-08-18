56d31cb45b7f037cd22b493a0872e46ca82f9ed5
Eric Plaster <plaster@gmail.com>
2011-08-18 14:27:34 -0500

    Removed MOFactory When methods are called off Managed Objects, arguments that are managed objects make sure the reference is used. The return values of methods now return pyvisdk managed objects instead of suds objects (except Retrieve Properties) If the name is passed in, but the ref isn't passed in to the managed object, it will search for the managed object. All managed objects have name as a property now. When updating a property, if the data returned is a list of one element, just the one element is set to the property. Added getDecendants method on all managed objects.

--- pyvisdk/base/base_entity.py
+++ pyvisdk/base/base_entity.py
@@ -14,30 +14,11 @@ import pyvisdk.do
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 
-def MOFactory(core, ref):
-    log.debug("REF: %s" % ref)
-    
-    # if we have an obj, then obj is the managed object reference
-    if hasattr(ref, 'obj'):
-        _class = eval("pyvisdk.mo.%s" % ref.obj._type)
-        return _class(core,  name=ref.propSet[0].val, ref=ref.obj)
-    
-    # check if ref is the managed object reference
-    elif (hasattr(ref, '_type') and hasattr(ref, 'value')) or ref.__class__.__name__ == 'ManagedObjectReference':
-        _class = eval("pyvisdk.mo.%s" % ref._type)
-        return _class(core,  ref=ref)
-    
-    # make sure we catch anything else...
-    else:
-        raise PyVisdkError("Unknown managed object reference: %s" % ref)
-
 class ManagedObjectReference(suds.sudsobject.Property):
     """Custom class to replace the suds generated class, which lacks _type."""
     def __init__(self, _type, value):
         suds.sudsobject.Property.__init__(self, value)
         self._type = _type
-        
-
 
 class TaskDelegate(object):
     """ delegates to the real method, and waits for the task to complete"""
@@ -46,22 +27,30 @@ class TaskDelegate(object):
         """ Initializes the delagate with the method to proxy """
         self.__target = getattr(cls.service, name)
         self.name = name
-        self.ref = cls.ref
-        self.core = cls.core
+        self.cls = cls
         
     def __call__(self, *args, **kwargs):
         """ calls original method """
         log.debug("Calling %s" % self.__target.method.name)
         if len(args):
-            args = list(args)
-            args.insert(0, self.ref)
+            _list = []
+            for x in list(args):
+                if x.__class__.__name__ in ManagedObjectTypes:
+                    _list.append(x.ref)
+                else:
+                    _list.append(x)
+            _list.insert(0, self.cls.ref)
+            args = _list
         else:
-            args = (self.ref, )
+            args = (self.cls.ref, )
         rv = self.__target(*args, **kwargs)
         if self.name[-5:] == '_Task':
             log.debug("Waiting for task to Complete")
-            self.core.waitForTask(rv)
-        return rv
+            self.cls.core.waitForTask(rv)
+        if self.__target.method.name == "RetrieveProperties":
+            return rv
+        else:
+            return self.cls.core._parse_object_content(rv)
 
     def _return_value(self, rv):
         if isinstance(rv, list):
@@ -84,7 +73,7 @@ class TaskDelegate(object):
 class BaseEntity(object):
     def __init__(self, core, name=None, ref=None, type=None):
         self.core = core
-        self.name = name
+        self._name = name
         self.ref = ref
         self.type = type
         
@@ -98,9 +87,23 @@ class BaseEntity(object):
             self.ref = ManagedObjectReference(self.type, self.ref.value)
     
             # we have a ref, AND we don't already have a name, we can get the name from it...
-            if not self.name:
-                self.name = self.ref.value
+            if not self._name:
+                self._name = self.ref.value
+        else:
+            if name:
+                self.ref = self.core.getDecendentsByName(self._name)
             
+    @property
+    def name(self):
+        '''List of custom field definitions that are valid for the object's type. The fields
+        are sorted by name.
+        '''
+        return self.update('name')
+    
+    @name.setter
+    def name(self, name):
+        self._name = name
+    
     def update(self, prop):
         if type(prop) == types.ListType:
             for x in prop:
@@ -108,11 +111,17 @@ class BaseEntity(object):
                 
         data = self.cache.get(prop, default=None)
         if not data:
-            data = self.core.getObjectProperties(self.ref, prop)
+            data = self.core.getObjectProperties(self.ref, prop, parent=self)
+            if isinstance(data, list) and len(data) == 1:
+                data = data[0]
             self.cache.set(prop, data)
             
+            if prop == 'name':
+                self._name = data
         return data
-                
+
+    def getDecendants(self, type=ManagedObjectTypes.ManagedEntity):
+        return self.core.getContentsRecursively(_type=type, root=self)
 
     def delegate(self, name):
         log.debug("Delegating %s" % name)
