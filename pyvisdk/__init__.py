1fe399755823f4bd4b826470a21e9e28658c023b
Eric Plaster <plaster@gmail.com>
2011-08-18 14:31:01 -0500

    Added the method new to pyvisdk to shortcut the creation of Vim objects.  Used mostly in testing.

--- pyvisdk/__init__.py
+++ pyvisdk/__init__.py
@@ -2,4 +2,11 @@ __version__ = '0.5'
 Version = __version__ # for backware compatibility
 
 from vim import Vim
+from app import PyvisdkApp
 from options import Options
+
+def new():
+    app = PyvisdkApp()
+    app.parse()
+    app.connect()
+    return app.vim
