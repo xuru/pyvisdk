eb1005b3f286e51e1e0e488f28461f92c323ac70
Eric Plaster <plaster@gmail.com>
2011-08-18 14:14:12 -0500

    Added camel_to_under function (converts camel case to underscore)

--- pyvisdk/utils.py
+++ pyvisdk/utils.py
@@ -3,3 +3,8 @@ Created on Jul 29, 2011
 
 @author: eplaster
 '''
+import re
+
+def camel_to_under(string):
+    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', string)
+    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
