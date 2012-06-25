d91bf8340978f1bea9cd5f351434695c0342ea21
Guy Rozendorn <guy@rzn.co.il>
2012-06-25 22:49:34 +0300

    TRIVIAL Added another test for the PropertyCollector facade, passed without modifications

--- tests/test_facade_whitebox.py
+++ tests/test_facade_whitebox.py
@@ -9,8 +9,9 @@ TOPOLOGY_PROPERTY_PATH = 'config.storageDevice.scsiTopology.adapter'
 HBA_KEY = "key-vim.host.ScsiTopology.Interface-vmhba0"
 TARGET_KEY = "key-vim.host.ScsiTopology.Target-vmhba0:0:0"
 LUN_KEY = "key-vim.host.ScsiTopology.Lun-020c0000006742b0f000004e2b0000000000000000496e66696e69"
+SCSI_LUNS_PROPERTY_PATH = "config.storageDevice.scsiLun"
 
-class PropertyCollectorFacadeTestCase(unittest.TestCase):
+class RegexTestCase(unittest.TestCase):
     def test_simple_property(self):
         result = findall(PROPERTY_NAME_PATTERN, 'foo.bar')
         self.assertEquals(result, ['foo', 'bar'])
@@ -30,6 +31,7 @@ class PropertyCollectorFacadeTestCase(unittest.TestCase):
                                    '["key-vim.host.ScsiTopology.Interface-vmhba2"]', 'target', 
                                    '["key-vim.host.ScsiTopology.Target-vmhba2:0:0"]'])
 
+class ScsiTopologyTestCase(unittest.TestCase):
     def _get_fake_property_collector(self, vim, filterSet=[]):
         fake = mock.Mock()
         update_set = Bunch(filterSet=filterSet, truncated=False, version='1')
@@ -156,6 +158,70 @@ class PropertyCollectorFacadeTestCase(unittest.TestCase):
         self._set_update_on_fake_property_collector(fake, [fourth_update])
         properties = facade.getProperties()
         self.assertEquals(properties["HostSystem:host-1"][TOPOLOGY_PROPERTY_PATH][0].target[0].lun, [])
+
+class ScsiLunsTestCase(unittest.TestCase):
+    def _get_fake_property_collector(self, vim, filterSet=[]):
+        fake = mock.Mock()
+        update_set = Bunch(filterSet=filterSet, truncated=False, version='1')
+        fake.WaitForUpdatesEx.return_value = update_set
+        vim.service_content.propertyCollector.CreatePropertyCollector.return_value = fake
+        return fake
+
+    def _set_update_on_fake_property_collector(self, fake, filterSet=[]):
+        version = str(int(fake.WaitForUpdatesEx.return_value.version) + 1)
+        update_set = Bunch(filterSet=filterSet, truncated=False, version=version)
+        fake.WaitForUpdatesEx.return_value = update_set
+
+    def _assert_fake_methods_were_called(self, vim, fake):
+        self.assertTrue(vim.service_content.propertyCollector.CreatePropertyCollector.called)
+        self.assertTrue(fake.WaitForUpdatesEx.called)
+
+    def test_get_properties__empty(self):
+        vim = mock.Mock()
+        fake = self._get_fake_property_collector(vim)
+        facade = HostSystemCachedPropertyCollector(vim, [SCSI_LUNS_PROPERTY_PATH])
+        properties = facade.getProperties()
+        self._assert_fake_methods_were_called(vim, fake)
+
+    def _get_initial_filter_update(self, vim):
+        managed_object_reference = Bunch(ref=Bunch(_type="HostSystem", value="host-1"))
+        lun_list = [Bunch(displayName="name", key=LUN_KEY)]
+        property_change = Bunch(name=SCSI_LUNS_PROPERTY_PATH, op="add", val=lun_list)
+        object_update = Bunch(kind="enter", missingSet=[], changeSet=[property_change], obj=managed_object_reference)
+        filter_update = Bunch(filter=None, missingSet=[], objectSet=[object_update])
+        return filter_update
+
+    def _get_second_filter_update(self, vim):
+        managed_object_reference = Bunch(ref=Bunch(_type="HostSystem", value="host-1"))
+        path = '{}["{}"].displayName'
+        property_change = Bunch(name=path.format(SCSI_LUNS_PROPERTY_PATH, LUN_KEY), op="assign", val="NAME")
+        object_update = Bunch(kind="modify", missingSet=[], changeSet=[property_change], obj=managed_object_reference)
+        filter_update = Bunch(filter=None, missingSet=[], objectSet=[object_update])
+        return filter_update
+
+    def test_get_properties__first_time(self):
+        vim = mock.Mock()
+        filter_update = self._get_initial_filter_update(vim)
+        fake = self._get_fake_property_collector(vim, [filter_update])
+        facade = HostSystemCachedPropertyCollector(vim, [SCSI_LUNS_PROPERTY_PATH])
+        properties = facade.getProperties()
+        self._assert_fake_methods_were_called(vim, fake)
+        self.assertEquals(properties.keys(), ["HostSystem:host-1"])
+        self.assertEquals(properties["HostSystem:host-1"][SCSI_LUNS_PROPERTY_PATH][0].key, LUN_KEY)
+        self.assertEquals(properties["HostSystem:host-1"][SCSI_LUNS_PROPERTY_PATH][0].displayName, "name")
+
+    def test_get_properties__display_name_changes(self):
+        vim = mock.Mock()
+        filter_update = self._get_initial_filter_update(vim)
+        fake = self._get_fake_property_collector(vim, [filter_update])
+        facade = HostSystemCachedPropertyCollector(vim, [SCSI_LUNS_PROPERTY_PATH])
+        properties = facade.getProperties()
+        second_update = self._get_second_filter_update(vim)
+        self._set_update_on_fake_property_collector(fake, [second_update])
+        properties = facade.getProperties()
+        self.assertEquals(properties.keys(), ["HostSystem:host-1"])
+        self.assertEquals(properties["HostSystem:host-1"][SCSI_LUNS_PROPERTY_PATH][0].key, LUN_KEY)
+        self.assertEquals(properties["HostSystem:host-1"][SCSI_LUNS_PROPERTY_PATH][0].displayName, "NAME")
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testHosts']
